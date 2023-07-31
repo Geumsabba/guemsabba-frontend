0ccc5cfb54146ca32c3e23bb2b6b181b84f41ab6
jinseo0315 <kjsys12173@gmail.com>
2023-07-31 21:41:36 +0900

    랜딩페이지 애니메이션 추가

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import "./Landing.css";
 import mainlogo from './mainlogo.png';
 
 export default function Landing() {
+  /* content에 fade-in */
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('show');
+        } else {
+          entry.target.classList.remove('show');
+        }
+      });
+    });
+
+    const sectionElements = document.querySelectorAll('.content');
+    sectionElements.forEach((el) => observer.observe(el));
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
   return (
-    <div>
-      <section>
+    <div className='fullpage'>
+      <section className='first'>
         <header>
           <ul>
             <li><a href="#">맞춤형 지원금</a></li>
-            <li><a href="#">맞춤형 재테크</a></li>
+            < li><a href="#">맞춤형 재테크</a></li>
             <li><a href="#">맞춤형 금융지식</a></li>
             <li><a href="#">회원가입</a></li>
           </ul>
@@ -31,7 +51,58 @@ export default function Landing() {
           </div>
         </div>
       </section>
+
+      <section className='second'>
+       <div className="content">
+        <div className="imgBox">
+          <img src={mainlogo} className="guemsabba" alt="main logo" />
+        </div>
+        <div className="textBox">
+          <h1>
+            지금 내가 받을 수 있는<br />
+            <span>지원금 혜택</span>은?
+          </h1>
+          <div className="button">
+            <a href="#">맞춤형 지원금 이동하기</a>
+          </div>
+        </div>
+       </div>
+      </section>
+
+      <section className='third'>
+      <div className="content">
+          <div className="textBox">
+            <h1>
+              나에게 딱 맞는<br />
+              <span>재테크 정보</span>는?
+            </h1>
+            <div className="button">
+              <a href="#">맞춤형 재테크 이동하기</a>
+            </div>
+          </div>
+          <div className="imgBox">
+            <img src={mainlogo} className="guemsabba" alt="main logo" />
+          </div>
+        </div>
+      </section>
+
+      <section className='forth'>
+      <div className="content">
+        <div className="imgBox">
+          <img src={mainlogo} className="guemsabba" alt="main logo" />
+        </div>
+        <div className="textBox">
+          <h1>
+            어려운 금융지식 !<br />
+            <span>금.사.빠</span>가<br />
+            도와줄게요 :)
+          </h1>
+          <div className="button">
+            <a href="#">맞춤형 금융지식 이동하기</a>
+          </div>
+        </div>
+      </div>
+      </section>
     </div>
   );
 }
-
